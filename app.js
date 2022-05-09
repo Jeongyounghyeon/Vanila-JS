866a5374e38f49071f7acf546b4734fb11b5c3bc
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-09 10:29:10 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var a = 5;
-var b = 2;
-var myName = "nico";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "nicolas";
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
