d511134c06bb234304741af538fc0c0c4b636f5b
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-10 14:42:32 +0900

    #2.10 Recap II

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const toBuy = ["postato", "tomato", "pizza"];
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  sub: function (a, b) {
+    console.log(a - b);
+  },
+  mul: function (a, b) {
+    console.log(a * b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+  squ: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-console.log(toBuy[2]);
-toBuy[2] = "water";
-console.log(toBuy);
-toBuy.push("one");
-console.log(toBuy);
+calculator.add(1, 2);
+calculator.sub(3, 4);
+calculator.mul(5, 6);
+calculator.div(7, 8);
+calculator.squ(9, 10);
