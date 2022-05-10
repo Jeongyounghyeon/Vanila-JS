ea362afb18daced8346a435fe546be83af8b4d52
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-10 15:37:22 +0900

    #2.13 Conditinoals

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  sub: function (a, b) {
-    return a - b;
-  },
-  mul: function (a, b) {
-    return a * b;
-  },
-  div: function (a, b) {
-    return a / b;
-  },
-  squ: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const addResult = calculator.add(2, 3);
-const substractResult = calculator.sub(addResult, 10);
-const mulipleResult = calculator.mul(10, substractResult);
-const divideResult = calculator.div(mulipleResult, addResult);
-const squeredResult = calculator.squ(divideResult, substractResult);
+console.log(age);
