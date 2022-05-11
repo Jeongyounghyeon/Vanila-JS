8be922363535e5f409d5a70bb9bbd86ca0eabd3a
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-11 16:04:18 +0900

    #3.0 The Document Object

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age)) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever ou want");
-}
+document.title = "Hello! From JS!";
