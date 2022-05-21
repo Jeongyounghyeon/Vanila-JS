f4676dfaa0683f6df74e3859cbe40fa777e26e34
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-21 16:02:12 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "e31525b0763e43d0c8d4e91204938afd";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live it", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang={lang}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-Child");
+      const city = document.querySelector("#weather span:last-Child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} `;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
