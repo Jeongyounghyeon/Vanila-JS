e543f458052f6e3c3ea959067c95488b52092ec6
Jeongyounghyeon <48899156+Jeongyounghyeon@users.noreply.github.com>
2022-05-21 14:52:20 +0900

    #7.5 Loading To Dos part Two

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem("todos", JSON.stringify(toDos));
@@ -18,13 +18,13 @@ function deleteToDo(event) {
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  toDoList.appendChild(li);
+  span.innerText = newTodo;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
@@ -40,7 +40,8 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if (saveToDos !== null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.foEach();
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
